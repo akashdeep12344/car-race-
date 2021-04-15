71780430af0ebcd61c57288dbb67d8b146c3895a
akashdeep12344 <bhangu.akashdeep007@gmail.com>
2021-04-15 18:04:31 +0530

    akashdeep12344

--- js/Game.js
+++ js/Game.js
@@ -33,10 +33,39 @@ class Game {
     textSize(30);
     text("Game Start", 120, 100)
     Player.getPlayerInfo();
+    
+    if(allPlayers!==undefined){
 
-    
-    
+   var display_position=130;
+   
+   for(var plr in allPlayers){
 
-    
+    if(plr==="player"+player.index) 
+
+
+  fill("brown");
+  else
+  fill("yellow");
+
+  display_position+=20;
+
+  textSize(32);
+
+  text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,display_position)
+
+    }
+ 
   }
+  if(keyIsDown(UP_ARROW)&&player.index!==null){
+
+    player.distance+=50;
+    player.update();
+
+   
+  }
+
+ 
+}
+  
+  
 }
